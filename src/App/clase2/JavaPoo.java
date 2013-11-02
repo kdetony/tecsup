a0b7214f35ab26ecdd91b9914a6191d076a48b14
alumno <alumno@LAB706-14>
2013-11-02 16:18:04 -0500

    Commit projects local

--- src/App/clase2/JavaPoo.java
+++ src/App/clase2/JavaPoo.java
@@ -2,7 +2,7 @@ package App.clase2;
 
 public class JavaPoo {
    public static void main(String[] args) {
-       System.out.println("Hola Mundo");     
+       System.out.println("Hola Mundo Java Tecsup 1");     
     }
    
 }

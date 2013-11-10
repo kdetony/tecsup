172d2b6375082a4e25f67e1b6e7b44b6438e33b3
alumno <alumno@LAB706-20>
2013-11-09 19:07:29 -0500

    formateo fechas

--- src/App/clase2/estatico/Estatico.java
+++ src/App/clase2/estatico/Estatico.java
@@ -3,6 +3,10 @@ package App.clase2.estatico;
 
 public class Estatico {
     public static int contador;
+
+    static double dolarEuro(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     public Estatico(){
